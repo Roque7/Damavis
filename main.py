dd14adf79153277ccd4025e0091a0d351888fc9c
roquegarcia <rgs.roque@gmail.com>
2023-07-25 12:02:27 +0200

    com pus

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+def juego_laberinto(laberinto):  # Función que calcula los movimientos necesarios en el laberinto
+    filas = len(laberinto)  # Guardo la cantidad de filas del laberinto
+    columnas = len(laberinto[0])  # Guardo la cantidad de columnas del laberinto
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint. hol
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    inicio = (0, 0, 'h')  # Posición de inicio con orientación horizontal
+    cola = [(inicio, 0)]  # Lista para almacenar las posiciones y movimientos
+    visitado = []  # Lista para registrar las posiciones visitadas
